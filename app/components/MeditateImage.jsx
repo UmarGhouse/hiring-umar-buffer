35dedef28f9f2a0b4245b422a98a119cf8580569
Umar Ghouse <m.umarghouse@gmail.com>
2023-12-18 18:53:03 +0530

    feat(parallax): Finish adding parallax to all 2d-and-3d images

---
+++ app/components/MeditateImage.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import { useParallax } from 'react-scroll-parallax';
+
+export default function MeditateImage() {
+  const { ref: cubeRef } = useParallax({ speed: 10, translateY: [50, -50] });
+  const { ref: icosaRef } = useParallax({ speed: 10, translateY: [-50, 50] });
+  const { ref: octaRef } = useParallax({ speed: 10, translateY: [50, -50] });
+
+  return (
+    <div className='relative'>
+      <Image src="/me/coworker/Atom_Cube_2_0001.png" alt='keyboard' width={150} height={150} className='absolute top-0 right-24 z-1' ref={cubeRef} />
+      <Image src="/me/coworker/Atom_Icosa_2_0003.png" alt='macbook' width={150} height={150} className='absolute z-10 left-0 top-20' ref={icosaRef} />
+      <Image src="/me/coworker/Atom_Octa_1_0003.png" alt='watch' width={150} height={150} className='absolute bottom-0 right-0 z-10' ref={octaRef} />
+      <Image src="/me/coworker/folks-young-woman-sitting-in-lotus-position-1.png" alt='Logo' width={500} height={500} className='relative z-2' />
+    </div>
+  )
+}
---
+++ app/components/RobotHandsImage.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import { useParallax } from 'react-scroll-parallax';
+
+export default function RobotHandsImage() {
+  const { ref: atomRef } = useParallax({ speed: 10, translateY: [150, -50] });
+  const { ref: sphereRef } = useParallax({ speed: 10, translateY: [50, -50] });
+  const { ref: springRef } = useParallax({ speed: 10, translateY: [-50, 50] });
+
+  return (
+    <div className='relative'>
+      <Image src="/me/coder/Atom_Tetra_2_0000.png" alt='keyboard' width={150} height={150} className='absolute bottom-20 right-10 z-1' ref={atomRef} />
+      <Image src="/me/coder/Sphere_Ring_Standing0003.png" alt='macbook' width={150} height={150} className='absolute z-10 left-0 top-40' ref={sphereRef} />
+      <Image src="/me/coder/Spring_Fat_Short0001.png" alt='watch' width={150} height={150} className='absolute top-0 right-0 z-1' ref={springRef} />
+      <Image src="/me/coder/folks-ai-coding-programming.png" alt='Logo' width={500} height={500} className='relative z-2' />
+    </div>
+  )
+}
